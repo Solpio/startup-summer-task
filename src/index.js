028464d72a6986d1eccda8ac30321c67494e1b83
Solpio <ssusolpio@gmail.com>
2022-05-25 15:24:39 +0400

    refactor: remove html layout from branch

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import "./styles/style.scss"
-console.log("Hello World!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-async function f(username){
-    const s = await fetch(`https://api.github.com/users/${username}`).then((value)=>{
-        console.log(value.json().then((value)=>{
-            console.log(value)
-        }));
-    });
-}
-
-let a = document.querySelector('.search_area');
-console.log(a); 
-a.addEventListener("keydown", (e)=>{
-    if (e.code === "Enter"){
-        e.preventDefault();
-        f(a.value);
-    }   
-})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
